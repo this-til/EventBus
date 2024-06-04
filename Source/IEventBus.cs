200b0f054f1cec99a0851663479c6f8c6cf814db
til <m15071452418@163.com>
2024-06-04 20:07:43 +0800

    0

--- Source/IEventBus.cs
+++ Source/IEventBus.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using log4net;
 
 namespace Til.EventBus {
     public interface IEventBus {
--- Source/ILog.cs
+++ Source/ILog.cs
@@ -2,10 +2,8 @@
 
 namespace Til.EventBus {
     public interface ILog {
-        void Debug(object message, Exception? exception = null);
-        void Info(object message, Exception? exception = null);
-        void Warn(object message, Exception? exception = null);
-        void Error(object message, Exception? exception = null);
-        void Fatal(object message, Exception? exception = null);
+        void Info(string message, Exception? exception = null);
+        void Warn(string message, Exception? exception = null);
+        void Error(string message, Exception? exception = null);
     }
 }
